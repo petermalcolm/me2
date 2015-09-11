f835f2f2c4d4893b301491c32f6de26be3574d47
petermalcolm <peter.t.malcolm@gmail.com>
2015-09-10 21:15:50 -0400

    second commit - now with files!

---
+++ js/app.js
@@ -0,0 +1,7 @@
+window.addEventListener('polymer-ready', function() {
+	var navicon = document.getElementById('navicon');
+	var drawerPanel = document.getElementById('drawerPanel');
+	navicon.addEventListener('click', function() {
+		drawerPanel.togglePanel();
+	});
+});
